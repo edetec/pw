60fcbe346d640c44bc56bf85faff30f6c644a9d9
Eder Lima Soares <edetec@gmail.com>
2014-10-21 22:15:02 -0200

    CRUD de canal concluído.

---
+++ guiaprogramacaoeletronica/src/br/senai/sc/ti20131n/pw/gpe/dao/CanalDao.java
@@ -0,0 +1,39 @@
+package br.senai.sc.ti20131n.pw.gpe.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.senai.sc.ti20131n.pw.gpe.entity.Canal;
+import br.senai.sc.ti20131n.pw.gpe.util.JpaUtil;
+
+public class CanalDao {
+	
+
+	private EntityManager entityManager;
+
+	public CanalDao() {
+		entityManager = JpaUtil.getEntityManager();
+	}
+	
+	public List<Canal> listar() {
+		Query query = entityManager
+				.createQuery("From Canal", Canal.class);
+		return query.getResultList();
+	}
+
+	public void salvar(Canal canal) {
+		entityManager.merge(canal);
+	}
+
+	public Canal buscarPorId(Long id) {
+		return entityManager.find(Canal.class, id);
+	}
+
+	public void excluir(Long id) {
+		Canal canal = entityManager.getReference(Canal.class, id);
+		entityManager.remove(canal);
+	}
+
+}
--- guiaprogramacaoeletronica/src/br/senai/sc/ti20131n/pw/gpe/mb/CanalMb.java
+++ guiaprogramacaoeletronica/src/br/senai/sc/ti20131n/pw/gpe/mb/CanalMb.java
@@ -2,57 +2,25 @@ package br.senai.sc.ti20131n.pw.gpe.mb;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.persistence.Query;
 
+import br.senai.sc.ti20131n.pw.gpe.dao.CanalDao;
 import br.senai.sc.ti20131n.pw.gpe.entity.Canal;
 import br.senai.sc.ti20131n.pw.gpe.util.JpaUtil;
 
 @ManagedBean
 public class CanalMb {
+	private CanalDao canalDao;
+	
 	private List<Canal> canais;
-	private String nome;
-	private String genero;
-	private Integer classificacao;
-	private Integer numero;
+	private Canal canal;
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-
-	public Integer getClassificacao() {
-		return classificacao;
-	}
-
-	public void setClassificacao(Integer classificacao) {
-		this.classificacao = classificacao;
-	}
-
-	public Integer getNumero() {
-		return numero;
-	}
-
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
 
 	public List<Canal> getCanais() {
 		if(canais == null){
-			Query query = JpaUtil.getEntityManager()
-					.createQuery("From Canal", Canal.class);
-			canais = query.getResultList();
+			canais = canalDao.listar();
 		}
 		return canais;
 	}
@@ -60,15 +28,35 @@ public class CanalMb {
 	public void setCanais(List<Canal> canais) {
 		this.canais = canais;
 	}
+
+	public Canal getCanal() {
+		return canal;
+	}
+
+	public void setCanal(Canal canal) {
+		this.canal = canal;
+	}
+	
+ //***************  ações *****************************
+	
+	@PostConstruct
+	public void init(){
+		canalDao = new CanalDao();
+		canal = new Canal();
+	}
 	
 	public String salvar(){
-		Canal canal = new Canal();
-		canal.setClassificacao(classificacao);
-		canal.setGenero(genero);
-		canal.setNome(nome);
-		canal.setNumero(numero);
-
-		JpaUtil.getEntityManager().persist(canal);
-		return "";
+		canalDao.salvar(getCanal());
+		return "canallista";
+	}
+	
+	public String carregarEdicao(Long id){
+		canal = canalDao.buscarPorId(id);
+		return "canalform";
+	}
+	
+	public String excluir(Long id){
+		canalDao.excluir(id);
+		return "canalform";
 	}
 }
